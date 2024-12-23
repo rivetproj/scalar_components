d2c7b46218acaf71c5653d1453d59f4b6431f04c
Mark Sharpley <698889+markopolo123@users.noreply.github.com>
2024-12-23 22:33:33 +0000

    fix(scalar.templ): fix struct
    Signed-off-by: Mark Sharpley <698889+markopolo123@users.noreply.github.com>

--- types.go
+++ types.go
@@ -2,11 +2,7 @@ package scalar
 
 type OptionsProp struct {
 	CDN       string `json:"cdn,omitempty"`
-	SpecURL   string `json:"specUrl,omitempty"` // allow external URL ou local path file
+	SpecURL   string `json:"specUrl,omitempty"`
 	CustomCss string `json:"customCss,omitempty"`
-	CustomOptions
-}
-
-type CustomOptions struct {
 	PageTitle string `json:"pageTitle,omitempty"`
 }
